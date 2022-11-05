1d1dbb661272d585c8154557cf17b000ed0951ab
Binh Nguyen <b.nguyen.sn@gmail.com>
2022-11-05 11:29:27 +0000

    Upgrade packages - Next.js to v13, etc. (#16)
    * feat: upgrade Next.js
    
    * feat: upgrade the remaining packages

--- components/InternalLink.js
+++ components/InternalLink.js
@@ -2,8 +2,8 @@ import NextLink from 'next/link';
 
 export default function InternalLink({ href, children }) {
   return (
-    <NextLink href={href}>
-      <a className="no-underline">{children}</a>
+    <NextLink href={href} className="no-underline">
+      {children}
     </NextLink>
   );
 }
