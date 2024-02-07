e10f6ffbd088bb7b3a1f0c7c7b220e83428ce785
Binh Nguyen <b.nguyen.sn@gmail.com>
2024-02-07 23:31:12 +0000

    chore: update packages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   swcMinify: true,
   images: {
@@ -10,4 +13,5 @@ module.exports = {
     });
     return config;
   },
+  output: 'export',
 };
